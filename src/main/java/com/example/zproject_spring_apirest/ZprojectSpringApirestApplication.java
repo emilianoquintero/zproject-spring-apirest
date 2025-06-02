17e9b2aaefb8b51a55fbd425bd1eef478893f33d
Emiliano <qb.emiliano@gmail.com>
2025-06-02 08:18:41 -0700

    first commit

---
+++ src/main/java/com/example/zproject_spring_apirest/ZprojectSpringApirestApplication.java
@@ -0,0 +1,13 @@
+package com.example.zproject_spring_apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZprojectSpringApirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZprojectSpringApirestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/zproject_spring_apirest/ZprojectSpringApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zproject_spring_apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZprojectSpringApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
